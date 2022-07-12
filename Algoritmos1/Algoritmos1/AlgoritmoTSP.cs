32229ca25bd89d12754f356a102da9d603cd7418
siul01 <103661464+siul01@users.noreply.github.com>
2022-07-12 17:00:29 -0400

    Add files via upload

---
+++ Algoritmos1/Algoritmos1/AlgoritmoTSP.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos1
+{
+    public class AlgoritmoTSP
+    {
+        private List<Nodo> _graph { get; set; }
+        private int _n = 0;
+        private Nodo _origen { get; set; }
+        private List<Ruta> _soluciones { get; set; }
+
+
+        public AlgoritmoTSP( List<Nodo> graph, int n, Nodo origen)
+        {
+            _graph = graph;
+            _n = n;
+            _origen = origen; 
+        }
+
+        public void Caminar()
+        {
+            _soluciones = new List<Ruta>();
+            for (int i = 0; i < _n; i++)
+            {
+                _soluciones.Add(GenerarRuta());
+            }
+        }
+
+        private Ruta GenerarRuta()
+        {
+            Ruta solucion = new Ruta();
+            solucion.Nodos.Add(_origen);
+            Nodo actual = _origen;
+            int soldados=1000;
+            for (int i = 0; i < _graph.Count - 1; i++)
+            {
+                Nodo siguiente = null;
+                do
+                {
+                    siguiente = SiguienteNodo(actual);
+
+                } while (solucion.Nodos.Contains(siguiente));
+                solucion.Nodos.Add(siguiente);
+                solucion.GastoOroTotal += actual.Caminos.Where(d => d.Nodo.Nombre == siguiente.Nombre).First().CostoOro;
+                solucion.MuertesTotal +=soldados * actual.Caminos.Where(d =>d.Nodo.Nombre == siguiente.Nombre).First().PorsentajeMuertes;
+                actual = siguiente;
+            }
+            solucion.Nodos.Add(_origen);
+            solucion.GastoOroTotal += actual.Caminos.Where(d => d.Nodo.Nombre == _origen.Nombre).First().CostoOro;
+            solucion.MuertesTotal +=actual.Caminos.Where(d => d.Nodo.Nombre == _origen.Nombre).First().PorsentajeMuertes;
+            return solucion;
+
+        }
+        
+        private Nodo? SiguienteNodo(Nodo actual)
+        {
+            int siguienteNodo = new Random().Next(0, +_graph.Count - 1);
+            return actual.Caminos[siguienteNodo].Nodo;
+        }
+
+        public string GetTodasRutas
+        {
+
+            get
+            {
+                int i = 1;
+                int oro = 20000;  //oro inicial de la expedicion
+                int OroFinal; //variable para almacenar el oro sobrante
+                int soldados = 1000;
+                double soldadossobrantes;
+                string result = string.Empty;
+                foreach (var ruta in _soluciones)
+                {
+                    foreach (var nodo in ruta.Nodos)
+                    {
+                        result += nodo.Nombre + "-";
+                    }
+                    OroFinal = oro - ruta.GastoOroTotal; //resta para saber cuanto le sobra
+                    soldadossobrantes = soldados - ruta.MuertesTotal;
+                    result += "Invacion N° "+i+".-  Costo de la invación  "+ ruta.GastoOroTotal + "\n" + 
+                        "  - El oro sobrante es de :" +OroFinal+ "\n" +
+                        "  -La cantidad de muertes es : "+ruta.MuertesTotal + "\n"+
+                        "  -Los soldados sobrantes son : " +soldadossobrantes+"\n \n" ;
+                    i++;
+                }
+              
+                return result;
+            }
+        }
+        public int MenorGasatoOro     //funcion para sacar la menor costo de cruzada
+        {
+            get
+            {
+                int menor = 10000;
+                foreach (var ruta in _soluciones)
+                {
+                    foreach (var nodo in ruta.Nodos)
+                    {
+                        if (ruta.GastoOroTotal < menor)
+                        {
+                            menor = ruta.GastoOroTotal;
+                        }
+                    }
+                   
+                }
+                return menor;
+
+            }
+
+        }
+        public double MenorMuertesSoldados     //funcion para sacar la menor baja de muestes
+        {
+            get
+            {
+                double menorMuerte = 10000;
+                foreach (var ruta in _soluciones)
+                {
+                    foreach (var nodo in ruta.Nodos)
+                    {
+                        if (ruta.MuertesTotal < menorMuerte)
+                        {
+                            menorMuerte = ruta.MuertesTotal;
+                        }
+                    }
+
+                }
+                return menorMuerte;
+
+            }
+
+        }
+
+    }
+}
---
+++ Algoritmos1/Algoritmos1/Program.cs
@@ -0,0 +1,74 @@
+﻿using Algoritmos1;
+using System.Data;
+using System.Diagnostics;
+
+
+
+Nodo DesemRey = new Nodo() { Nombre = "DesemRey" };
+Nodo Braavos = new Nodo() { Nombre = "Braavos" };
+Nodo Qarth = new Nodo() { Nombre = "Qarth" };
+Nodo Asshai = new Nodo() { Nombre = "Asshai" };
+Nodo Meereen = new Nodo() { Nombre = "Meereen" };
+Nodo Pentos = new Nodo() { Nombre = "Pentos" };
+Nodo Volantis = new Nodo() { Nombre = "Volantis" };
+
+
+DesemRey.Caminos.Add( new Camino() { Nodo = Braavos, CostoOro = 1200 ,PorsentajeMuertes=0.07 });
+DesemRey.Caminos.Add( new Camino() { Nodo = Qarth, CostoOro = 1900 , PorsentajeMuertes = 0.09  });
+DesemRey.Caminos.Add( new Camino() { Nodo = Asshai, CostoOro = 1050, PorsentajeMuertes = 0.01 });
+DesemRey.Caminos.Add(new Camino() { Nodo = Meereen, CostoOro = 900, PorsentajeMuertes = 0.03 });
+DesemRey.Caminos.Add(new Camino() { Nodo = Pentos, CostoOro = 950, PorsentajeMuertes = 0.04 });
+DesemRey.Caminos.Add(new Camino() { Nodo = Volantis, CostoOro = 1600, PorsentajeMuertes = 0.06 });
+
+Braavos.Caminos.Add(new Camino() { Nodo = DesemRey, CostoOro = 1200, PorsentajeMuertes = 0.00 });
+Braavos.Caminos.Add(new Camino() { Nodo = Qarth, CostoOro = 1900, PorsentajeMuertes = 0.11 });
+Braavos.Caminos.Add(new Camino() { Nodo = Asshai, CostoOro = 1000, PorsentajeMuertes = 0.12 });
+Braavos.Caminos.Add(new Camino() { Nodo = Meereen, CostoOro = 1800, PorsentajeMuertes = 0.02 });
+Braavos.Caminos.Add(new Camino() { Nodo = Pentos, CostoOro = 1300, PorsentajeMuertes = 0.04 });
+Braavos.Caminos.Add(new Camino() { Nodo = Volantis, CostoOro = 1000, PorsentajeMuertes = 0.02 });
+
+Qarth.Caminos.Add(new Camino() { Nodo = DesemRey, CostoOro = 1900, PorsentajeMuertes = 0.00 });
+Qarth.Caminos.Add(new Camino() { Nodo = Braavos, CostoOro = 1900, PorsentajeMuertes = 0.11 });
+Qarth.Caminos.Add(new Camino() { Nodo = Asshai, CostoOro = 500, PorsentajeMuertes = 0.07 });
+Qarth.Caminos.Add(new Camino() { Nodo = Meereen, CostoOro = 700, PorsentajeMuertes = 0.06 });
+Qarth.Caminos.Add(new Camino() { Nodo = Pentos, CostoOro = 1200, PorsentajeMuertes = 0.03 });
+Qarth.Caminos.Add(new Camino() { Nodo = Volantis, CostoOro = 1050, PorsentajeMuertes = 0.03 });
+
+Asshai.Caminos.Add(new Camino() { Nodo = DesemRey, CostoOro = 1050, PorsentajeMuertes = 0.00 });
+Asshai.Caminos.Add(new Camino() { Nodo = Braavos, CostoOro = 1000, PorsentajeMuertes = 0.12 });
+Asshai.Caminos.Add(new Camino() { Nodo = Qarth, CostoOro = 500, PorsentajeMuertes = 0.07 });
+Asshai.Caminos.Add(new Camino() { Nodo = Meereen, CostoOro = 300, PorsentajeMuertes = 0.05 });
+Asshai.Caminos.Add(new Camino() { Nodo = Pentos, CostoOro = 1100, PorsentajeMuertes = 0.02 });
+Asshai.Caminos.Add(new Camino() { Nodo = Volantis, CostoOro = 1100, PorsentajeMuertes = 0.08 });
+
+
+Meereen.Caminos.Add(new Camino() { Nodo = DesemRey, CostoOro = 900, PorsentajeMuertes = 0.00 });
+Meereen.Caminos.Add(new Camino() { Nodo = Braavos, CostoOro = 1800, PorsentajeMuertes = 0.02 });
+Meereen.Caminos.Add(new Camino() { Nodo = Qarth, CostoOro = 700, PorsentajeMuertes = 0.06 });
+Meereen.Caminos.Add(new Camino() { Nodo = Asshai, CostoOro = 300, PorsentajeMuertes = 0.05 });
+Meereen.Caminos.Add(new Camino() { Nodo = Pentos, CostoOro = 200, PorsentajeMuertes = 0.15 });
+Meereen.Caminos.Add(new Camino() { Nodo = Volantis, CostoOro = 1600, PorsentajeMuertes = 0.01 });
+
+
+Pentos.Caminos.Add(new Camino() { Nodo = DesemRey, CostoOro = 950, PorsentajeMuertes = 0.00 });
+Pentos.Caminos.Add(new Camino() { Nodo = Braavos, CostoOro = 1300, PorsentajeMuertes = 0.04 });
+Pentos.Caminos.Add(new Camino() { Nodo = Qarth, CostoOro = 1200, PorsentajeMuertes = 0.03 });
+Pentos.Caminos.Add(new Camino() { Nodo = Asshai, CostoOro = 1100, PorsentajeMuertes = 0.02 });
+Pentos.Caminos.Add(new Camino() { Nodo = Meereen, CostoOro = 200, PorsentajeMuertes = 0.15 });
+Pentos.Caminos.Add(new Camino() { Nodo = Volantis, CostoOro = 800, PorsentajeMuertes = 0.03 });
+
+
+Volantis.Caminos.Add(new Camino() { Nodo = DesemRey, CostoOro = 1600, PorsentajeMuertes = 0.00 });
+Volantis.Caminos.Add(new Camino() { Nodo = Braavos, CostoOro = 1000, PorsentajeMuertes = 0.02 });
+Volantis.Caminos.Add(new Camino() { Nodo = Qarth, CostoOro = 1050, PorsentajeMuertes = 0.03 });
+Volantis.Caminos.Add(new Camino() { Nodo = Asshai, CostoOro = 1100, PorsentajeMuertes = 0.08 });
+Volantis.Caminos.Add(new Camino() { Nodo = Meereen, CostoOro = 1600, PorsentajeMuertes = 0.01 });
+Volantis.Caminos.Add(new Camino() { Nodo = Pentos, CostoOro = 800, PorsentajeMuertes = 0.03 });
+
+
+List<Nodo> graph = new List<Nodo>() { DesemRey, Braavos, Qarth, Asshai, Meereen, Pentos, Volantis };
+
+AlgoritmoTSP tsp = new AlgoritmoTSP(graph, 20, DesemRey);
+tsp.Caminar();
+Console.WriteLine(tsp.GetTodasRutas);
+Console.WriteLine("El menor costo en oro es "+tsp.MenorGasatoOro +"  y la menor taza de muerte de soldados es de "+tsp.MenorMuertesSoldados); 
