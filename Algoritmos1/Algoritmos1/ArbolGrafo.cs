ddef8e9b658c59c7a00078639e792884a52634b1
siul01 <103661464+siul01@users.noreply.github.com>
2022-07-13 00:19:15 -0400

    Add files via upload

---
+++ Algoritmos1/Algoritmos1/ArbolGrafo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos1
+{
+    internal class ArbolGrafo
+    {
+        public NodoGrafo Raiz { get; set; }
+        public ArbolGrafo()
+        {
+            Raiz = null;
+        }
+        public void Agregar(NodoGrafo n)
+        {
+            NodoGrafo nodo = new NodoGrafo(n.Info);
+            Raiz = AgregarW(nodo, Raiz);
+        }
+
+        public NodoGrafo AgregarW(NodoGrafo ng, NodoGrafo raiz)
+        {
+            if (raiz == null)
+            {
+                raiz = ng;
+            }
+            else if(ng.Info < raiz.Info)
+            {
+                raiz.NodoIzq = AgregarW(ng, raiz.NodoIzq);
+            }
+            else
+            {
+                raiz.NodoDer = AgregarW(ng, raiz.NodoDer);
+
+            }
+            return raiz;
+        }
+
+        
+        public int Imprimir(NodoGrafo ng)
+        {
+            if (ng == null)
+            {
+                return 0;
+            }
+
+            //  [Izq | Info | Der ]
+            string cad = string.Format("[{0}|{1}|{2}]", 
+                    ng.NodoIzq == null ?"null": ng.NodoIzq.Info,
+                    ng.Info,
+                    ng.NodoDer == null ?"null": ng.NodoDer.Info);
+            Console.WriteLine(cad);
+            Imprimir(ng.NodoIzq);
+            Imprimir(ng.NodoDer);
+            return 0;
+        }
+
+        // Implementar un algoritmo de busqueda en arboles binarios
+        public NodoGrafo Buscar(int buscar, NodoGrafo ng)
+        {
+            if (ng != null)
+            {
+                if (buscar < ng.Info)
+                {
+                    Buscar(buscar,ng.NodoIzq);
+               
+                }
+                else if (buscar > ng.Info)
+                {
+                    Buscar(buscar, ng.NodoDer);
+                
+                }
+                string cad = string.Format("[{0}|{1}|{2}]",
+               ng.NodoIzq == null ? "null" : ng.NodoIzq.Info,
+               ng.Info,
+               ng.NodoDer == null ? "null" : ng.NodoDer.Info);
+                Console.WriteLine(cad);
+
+            }
+            else
+            {
+                Console.WriteLine("El numero "+buscar+" no existe en el arbol ");
+            }
+
+            return ng;
+        }
+    }
+}
---
+++ Algoritmos1/Algoritmos1/Program.cs
@@ -0,0 +1,30 @@
+﻿using Algoritmos1;
+using System.Data;
+using System.Diagnostics;
+
+
+Console.WriteLine("Ejemplo arbol binario");
+ArbolGrafo arbol = new ArbolGrafo();
+arbol.Agregar(new NodoGrafo(5));
+arbol.Agregar(new NodoGrafo(4));
+arbol.Agregar(new NodoGrafo(3));
+arbol.Agregar(new NodoGrafo(2));
+arbol.Agregar(new NodoGrafo(1));
+arbol.Agregar(new NodoGrafo(16));
+arbol.Agregar(new NodoGrafo(8));
+arbol.Agregar(new NodoGrafo(6));
+arbol.Agregar(new NodoGrafo(9));
+arbol.Agregar(new NodoGrafo(22));
+arbol.Agregar(new NodoGrafo(17));
+arbol.Agregar(new NodoGrafo(25));
+arbol.Agregar(new NodoGrafo(23));
+
+arbol.Imprimir(arbol.Raiz);
+
+Console.WriteLine("Ingrese elemento a buscar ");
+int x = int.Parse(Console.ReadLine());
+NodoGrafo nb = arbol.Buscar(x,arbol.Raiz);
+
+
+
+Console.WriteLine();
